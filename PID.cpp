4b3825512b7836fae3454000416617338b4398c6
Erusa <eca072@gmail.com>
2021-11-04 21:26:06 +0100

    pid written and tunned with throttle 0.3

--- PID.cpp
+++ PID.cpp
@@ -4,7 +4,15 @@
  * TODO: Complete the PID class. You may add any additional desired functions.
  */
 
-PID::PID() {}
+PID::PID() {
+  Kp = 0;
+  Ki = 0;
+  Kd = 0;
+  
+  p_error = 0;
+  i_error = 0;
+  d_error = 0;
+}
 
 PID::~PID() {}
 
@@ -12,19 +20,35 @@ void PID::Init(double Kp_, double Ki_, double Kd_) {
   /**
    * TODO: Initialize PID coefficients (and errors, if needed)
    */
-
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+  
+  //p_error = 0;
+  //i_error = 0;
+  //d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
   /**
    * TODO: Update PID errors based on cte.
    */
-
+  double prev_cte = p_error;
+  p_error = cte;
+  i_error += cte;
+  d_error = cte - prev_cte;
 }
 
-double PID::TotalError() {
+double PID::TotalError(double cte) {
   /**
    * TODO: Calculate and return the total error
    */
-  return 0.0;  // TODO: Add your total error calc here!
+  double totalError;
+  UpdateError(cte);
+  totalError = -p_error*Kp - i_error*Ki - d_error*Kd;
+  
+  if (totalError > 1){ totalError = 1;}
+  else if (totalError < -1) {totalError = -1;}
+  
+  return totalError;  // TODO: Add your total error calc here!
 }
--- main.cpp
+++ main.cpp
@@ -34,10 +34,12 @@ int main() {
   uWS::Hub h;
 
   PID pid;
+  
   /**
    * TODO: Initialize the pid variable.
    */
-
+	pid.Init(0.1, 0.001, 1); //0.1, 0.0, 1 KP =0.05 Kd= 0.5 --> probiert große
+  
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, 
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -63,6 +65,8 @@ int main() {
            * NOTE: Feel free to play around with the throttle and speed.
            *   Maybe use another PID controller to control the speed!
            */
+          steer_value = pid.TotalError(cte);
+          
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value 
